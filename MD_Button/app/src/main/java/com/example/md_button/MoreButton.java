bb96699470c440123728bf77912d3435bd6506d3
Marium <Mariumyounus@aptech.com>
2023-06-08 12:27:40 +0500

    Android Lecture

---
+++ MD_Button/app/src/main/java/com/example/md_button/MoreButton.java
@@ -0,0 +1,14 @@
+package com.example.md_button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoreButton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_button);
+    }
+}
